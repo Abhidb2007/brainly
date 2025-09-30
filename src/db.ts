08f3bb234c223998e98d9aca5d11a2b6c7a65b7c
Abhidb2007 <abhishekdb2210@gmail.com>
2025-09-30 21:24:11 +0530

    code

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,16 @@
-import {model, Schema}  from "mongoose";
-mongooose.connect("mongodb://localhost:27017/brainly");   
-
+import mongoose, { model, Schema } from "mongoose";
+// Define schema
 const UserSchema = new Schema({
   username: { type: String, unique: true },
   password: String,
 });
 
-export const UserModel =model("User", UserSchema);
+// Create model
+export const UserModel = model("User", UserSchema);
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  tags: [{type: mongoose.Types.ObjectId, ref:"Tag"}],
+  userId:{ type: mongoose.Types.ObjectId, ref: "User",requires: true}
+});
+export const ContentModel=model("Content",ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -22,12 +22,48 @@ app.post("/api/v1/signup",(req, res)=>{
 
 })
 app.post("/api/v1/signin",(req, res)=>{
+    const username=req.body.username;
+    const password=req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if(existingUser){
+        const token=jwt.sign({
+            id:existingUser._id
+        },JWT_PASSWORD)
+        res.json({
+            token
+        })
+    }else{
+        res.status(401).json({
+            message:"Invalid credentials"
+        })
+    }
 
 })
-app.post("/api/v1/content",(req, res)=>{
+app.post("/api/v1/content",userMiddleware, async(req, res)=>{
+    (property) Response<any, Record<string,any>,number>,json;
+    (body?:any)=> Response<any, Record<string, any>, number>
+    Send JSON response.
+    Examples:
+    res.json(null);
+    res.json({user:"tj"});
+    res.status(500).json("oh noes!");
+    res.status(401).json("I don't have that");
+    res.json({
+        message: "Content added"
+    })
 
 })
-app.get("/api/v1/content",(req, res)=>{
+app.get("/api/v1/content",usemiddleware, aync(req, res)=>{
+    const userId=requestAnimationFrame.userId;
+    const content=await ContentModel.find({
+        userId: userId
+    }).populate("userId","username");
+    res.json({
+        content
+    })
 
 })
 app.delete("/api/v1/content",(req, res)=>{
